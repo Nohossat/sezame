85b8b22d759bcd60b5223534ec959eabf847c5ca
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-10-02 11:53:53 +0200

    clean songs files names

--- clean_filename.py
+++ clean_filename.py
@@ -1,1 +1,19 @@
 import os
+from os import listdir
+import re
+
+path = "songs/"
+files = listdir(path)
+
+count = 0
+
+for file in files:
+    pattern = re.compile(r"\d{1,2}\s*(.+)")
+    result = pattern.search(file)
+
+    if result:
+        count += 1
+        print(result.group(1))
+        os.rename(os.path.join(path, file), os.path.join(path,result.group(1)))
+
+print(count)
